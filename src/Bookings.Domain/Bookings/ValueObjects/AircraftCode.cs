205e19ee3288854192a2d03b54e8f0fab84a2995
ArtemToporkov <toporkovartem259@gmail.com>
2025-09-19 18:56:51 +0500

    refactor: use const for aircraft code length

--- src/Bookings.Domain/Bookings/ValueObjects/AircraftCode.cs
+++ src/Bookings.Domain/Bookings/ValueObjects/AircraftCode.cs
@@ -4,6 +4,7 @@ namespace Bookings.Domain.Bookings.ValueObjects;
 
 public readonly record struct AircraftCode : IStringBackedData<AircraftCode>
 {
+    public const int AircraftCodeLength = 3;
     private readonly string _value;
     
     private AircraftCode(string value) => _value = value;
@@ -11,7 +12,7 @@ public readonly record struct AircraftCode : IStringBackedData<AircraftCode>
     public static AircraftCode FromString(string @string)
     {
         if (@string.Length != 3)
-            throw new FormatException($"{nameof(AircraftCode)} should contain exactly three characters");
+            throw new FormatException($"{nameof(AircraftCode)} should contain exactly {AircraftCodeLength} characters");
         for (var i = 0; i < @string.Length; i++)
             if (!char.IsDigit(@string[i]) && (@string[i] < 'A' || @string[i] > 'Z'))
                 throw new FormatException($"{nameof(AircraftCode)} must consist only of numbers and letters A-Z). " +

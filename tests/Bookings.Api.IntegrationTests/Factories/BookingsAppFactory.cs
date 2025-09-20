708a83ec54e61b0c0312080ed878800643b1fead
ArtemToporkov <toporkovartem259@gmail.com>
2025-09-20 14:05:44 +0500

    fix: transaction is not used
    API request handler requests DbContext from DI so it does not use test's transaction

--- tests/Bookings.Api.IntegrationTests/Factories/BookingsAppFactory.cs
+++ tests/Bookings.Api.IntegrationTests/Factories/BookingsAppFactory.cs
@@ -1,4 +1,5 @@
-﻿using Bookings.Api.IntegrationTests.Extensions;
+﻿using System.Data.Common;
+using Bookings.Api.IntegrationTests.Extensions;
 using Bookings.Api.IntegrationTests.Mocks;
 using Bookings.Application.Bookings.Services;
 using Bookings.Infrastructure.Persistence;
@@ -7,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Npgsql;
 
 namespace Bookings.Api.IntegrationTests.Factories;
 
@@ -14,14 +16,31 @@ public class BookingsAppFactory : WebApplicationFactory<Program>
 {
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
+        builder.UseEnvironment("Development");
         builder.ConfigureServices(services =>
         {
             // NOTE: this is a real database that is being used in production
             // consider replacing with test database
             
+            services.RemoveServiceDescriptorIfExists<DbContextOptions<BookingsDbContext>>();
             services.RemoveServiceDescriptorIfExists<BookingsDbContext>();
-            services.AddNpgsql<BookingsDbContext>(
-                "Host=localhost;Port=5432;Database=DreamFly;Username=postgres;Password=postgres");
+            
+            // NOTE: the connection in the API request handler should be the same as in 
+            // BaseDatabaseIntegrationTest to ensure it runs within the test's transaction
+            // normally, we should use a test database that resets for every test (check Respawn or an analog)
+
+            services.AddSingleton<DbConnection>(_ =>
+            {
+                var connection = new NpgsqlConnection(
+                    "Host=localhost;Port=5432;Database=DreamFly;Username=postgres;Password=postgres");
+                connection.Open();
+                return connection;
+            });
+            services.AddDbContext<BookingsDbContext>((container, options) =>
+            {
+                var connection = container.GetRequiredService<DbConnection>();
+                options.UseNpgsql(connection);
+            });
             
             services.ReplaceSingletonService<IStringBackedDataGeneratorService, 
                 MockConstStringBackedDataGeneratorService>();
